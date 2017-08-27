a4eddf8b70b3a56afb5deb44be927631905f7bd0
colleowino <colleowino@gmail.com>
2017-08-27 14:02:32 +0300

    able to fetch results from user entered page

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const mkdirp = require('mkdirp');
 const path = require('path');
 
 var url = "https://www.lapa.ninja"
+var pgNum = process.argv[2];
 
 // Promise interface
 function scrapeListPage(pgNum){
+	console.log("Downloading page: "+pgNum);
+
 	scrapeIt(url+"/page/"+pgNum, {
 			// fetch the screenshot pages
 			pagelinks: {
@@ -20,42 +23,47 @@ function scrapeListPage(pgNum){
 				}
 			}
 	}).then(page => {
-			console.log(page);
+		var found = page.pagelinks;
+		for(var i = 0; i < found.length; i++){
+			scrapePostPage(found[i].post);
+		}
 	});
 }
 
-var linkpage = "https://www.lapa.ninja/post/frederique-matti/"
 
 function scrapePostPage(link){
+	//console.log("fetching: "+link);
+
 	scrapeIt(link, {
 		imageLink : {
 			selector: ".detail-post__img img",
 			attr: "src"
 		}
 	}).then( res => {
-		console.log(url+res.imageLink);
+		var img = url+res.imageLink;
+		downloadImage(img);
 	});
 }
 
-var img_test = "https://www.lapa.ninja/assets/images/Frederique-Matti.jpg" 
-
-function downloadImage(pgNum,imglink){
+function downloadImage(imglink){
 	var folder = path.join(__dirname,"downloads", pgNum.toString());
 
 	mkdirp(folder,function(err){
 		if(err) console.error(err)
-		else console.log("folder created: "+pgNum);
+		//else console.log("folder created: "+pgNum);
 	});
 
 	download.image({url: imglink, dest: folder})
 		.then(({ filename, image }) => {
-			console.log('File saved to', filename)
+			console.log('completed ', path.basename(filename))
 		}).catch((err) => {
 			throw err
 		})
 
 }
 
+//var img_test = "https://www.lapa.ninja/assets/images/Frederique-Matti.jpg" 
 //downloadImage(2, img_test);
+//var linkpage = "https://www.lapa.ninja/post/frederique-matti/"
 //scrapePostPage(linkpage);
-//scrapeListPage(2);
+scrapeListPage(pgNum);
